d20c6ea4469faf2eee7bed3efbd903f18cd5390c
thaikw <thaina.lima47@etec.sp.gov.br>
2025-09-17 17:14:35 -0300

    Classe e Atributos

---
+++ 01conceitos/Pessoa.cs
@@ -0,0 +1,18 @@
+class Pessoa
+{
+    // Atributos 
+    public string? Nome;
+    public int Idade;
+
+    // Método de apresentação
+    public void Apresentacao()
+    {
+        Console.WriteLine($"Olá, meu nome é: {Nome}");
+    }
+
+    // Método para retornar a situação da idade
+    public string VerificarIdade()
+    {
+        return Idade >= 18 ? "Maior de idade" : "Menor de idade";
+    }
+}
---
+++ 01conceitos/Program.cs
@@ -0,0 +1,19 @@
+﻿//Exemplo 01
+Pessoa objeto1 = new Pessoa();
+objeto1.Nome = "Thainá";
+//objeto1.Apresentacao();
+
+//Exemplo02
+Pessoa objeto2 = new();
+objeto2.Nome = "Sandro";
+//objeto2.Apresentacao();
+
+//Exemplo3
+Pessoa objeto3 = new()
+{
+    Nome = "Fabiana",
+    Idade = 23
+};
+//Verifica a idade usando o método criado na classe Pessoa
+string retorno = objeto3.VerificarIdade();
+Console.WriteLine($"A pessoa é {retorno}");
